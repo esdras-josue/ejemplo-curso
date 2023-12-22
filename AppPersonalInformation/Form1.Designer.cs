d425fcd6fb621f3ef9c515c9fe941a7ea7b4e81d
esdras-josue <esdraspea@gmail.com>
2023-12-22 09:49:22 -0600

    primera subida

---
+++ AppPersonalInformation/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AppPersonalInformation
+{
+    partial class AppInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDirreccion = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnAcceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(51, 41);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(74, 16);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "APELLIDO:";
+            this.lblApellido.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(131, 41);
+            this.txtApellido.MaxLength = 12;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(558, 22);
+            this.txtApellido.TabIndex = 1;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(51, 91);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "NOMBRE:";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(131, 88);
+            this.txtNombre.MaxLength = 12;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(558, 22);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(51, 136);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(48, 16);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "EDAD:";
+            this.lblEdad.Click += new System.EventHandler(this.lblEdad_Click);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(131, 133);
+            this.txtEdad.MaxLength = 2;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(117, 22);
+            this.txtEdad.TabIndex = 5;
+            this.txtEdad.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(51, 179);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(83, 16);
+            this.lblDireccion.TabIndex = 6;
+            this.lblDireccion.Text = "DIRECCION:";
+            // 
+            // txtDirreccion
+            // 
+            this.txtDirreccion.Location = new System.Drawing.Point(140, 176);
+            this.txtDirreccion.Name = "txtDirreccion";
+            this.txtDirreccion.Size = new System.Drawing.Size(542, 22);
+            this.txtDirreccion.TabIndex = 7;
+            this.txtDirreccion.TextChanged += new System.EventHandler(this.txtDirreccion_TextChanged);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(53, 227);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(93, 16);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "RESULTADO:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(127, 259);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(570, 113);
+            this.txtResultado.TabIndex = 9;
+            this.txtResultado.TextChanged += new System.EventHandler(this.txtResultado_TextChanged);
+            // 
+            // btnAcceptar
+            // 
+            this.btnAcceptar.Location = new System.Drawing.Point(269, 393);
+            this.btnAcceptar.Name = "btnAcceptar";
+            this.btnAcceptar.Size = new System.Drawing.Size(90, 28);
+            this.btnAcceptar.TabIndex = 10;
+            this.btnAcceptar.Text = "Aceptar";
+            this.btnAcceptar.UseVisualStyleBackColor = true;
+            this.btnAcceptar.Click += new System.EventHandler(this.btnAcceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(455, 393);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(91, 28);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // AppInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(764, 451);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAcceptar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDirreccion);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.MaximumSize = new System.Drawing.Size(794, 520);
+            this.MinimizeBox = false;
+            this.Name = "AppInformation";
+            this.Text = "Personal Information";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.btnCancelar_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtDirreccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnAcceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ AppPersonalInformation/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppPersonalInformation
+{
+    public partial class AppInformation : Form
+    {
+        public AppInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+           string apellido = txtApellido.Text;
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEdad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDirreccion_TextChanged(object sender, EventArgs e)
+        {
+            string direccion = txtDirreccion.Text;
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+            string edad = txtEdad.Text;
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAcceptar_Click(object sender, EventArgs e)
+        {
+            string apellido = txtApellido.Text;
+            string nombre = txtNombre.Text;
+            string edad = txtEdad.Text;
+            string direccion = txtDirreccion.Text;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(apellido) || string.IsNullOrWhiteSpace(edad))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Sale del método sin procesar la información
+            }
+
+            txtResultado.Text = "Nombre: " + nombre + Environment.NewLine +
+                                  "Apelldio: " + apellido + Environment.NewLine +
+                                  "Edad: " + edad + Environment.NewLine +
+                                  "Direccion: " + direccion + Environment.NewLine;
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCancelar_Click(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true; // Suprime la tecla si no es un número o retroceso
+            }
+        }
+    }
+}
